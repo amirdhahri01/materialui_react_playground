df25ff5f249d9ff8252d4dbdccc0396ad7a6edea
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-16 18:28:11 +0100

    An update

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,9 +1,15 @@
+import { Typography } from '@mui/material'
 import { Outlet } from 'react-router-dom'
 
 const Root = () => {
     return (
         <div>
-            <header></header>
+            <header>
+                <Typography variant="h4" component="p">
+                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi adipisci ducimus, possimus velit voluptas quisquam temporibus optio quis, cumque quibusdam fuga perspiciatis sed voluptatibus magnam. Voluptas veniam voluptatibus eius expedita.
+                </Typography>
+
+            </header>
             <Outlet />
         </div>
     )
