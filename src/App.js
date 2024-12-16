65b0188dd48952cd9a9d2f4bcbff8f45a23916d2
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-16 17:44:05 +0100

    An update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { createBrowserRouter, createRoutesFromElements, Route , RouterProvider} from 'react-router-dom';
 import './App.css';
+import Root from './Pages/Root';
 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Root/>}>
+      <Route path='dashboard' element={<Dashboard />}/>
+
+    </Route>
+  )
+)
 function App() {
   return (
     <div className="App">
+
     </div>
   );
 }
