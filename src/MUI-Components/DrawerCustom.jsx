3a39e9e2d23f0822b0e801719e9a8f15ab4d585d
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-19 17:43:36 +0100

    An update

--- src/MUI-Components/DrawerCustom.jsx
+++ src/MUI-Components/DrawerCustom.jsx
@@ -1,4 +1,4 @@
-import { Divider, Drawer, Toolbar } from '@mui/material'
+import {Divider, Drawer, Toolbar} from '@mui/material'
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
